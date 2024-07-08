53ac13141163f9d1c40eb6a73843ddc0bb4b88cd
MontseCamch <montserratcamch@gmail.com>
2024-07-07 21:37:36 -0600

    App Alura Cinema V1

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import Favoritos from "pages/Favoritos";
+import NotFound from "pages/NotFound";
+import PaginaBase from "pages/PaginaBase";
+import Player from "pages/Player";
+
+const { default: Inicio } = require("pages/Inicio");
+const { BrowserRouter, Routes, Route } = require("react-router-dom");
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="favoritos" element={<Favoritos />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
+                    <Route patch="*" element={<NotFound />}></Route>
+                </Route>
+            </Routes>
+
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
